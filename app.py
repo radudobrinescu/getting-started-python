7c42f4a17e4873afe8fdd0a697f8dff24620bf6f
radudobrinescu <radu.dobrinescu@gmail.com>
2018-07-23 20:10:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,6 @@ def cities():
     return resp
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
